c677d82ac3bc1461f143b5ffd60dabb70eb068ca
Riyaz <Riyaz@DESKTOP-V72SVI0>
2022-06-01 12:29:30 +0530

    commit

--- springbootfirst/src/main/java/com/springbootfirst/springbootfirst/SpringbootfirstApplication.java
+++ springbootfirst/src/main/java/com/springbootfirst/springbootfirst/SpringbootfirstApplication.java
@@ -3,6 +3,7 @@ package com.springbootfirst.springbootfirst;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //
+//
 @SpringBootApplication
 public class SpringbootfirstApplication {
 
